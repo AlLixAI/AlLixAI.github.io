75191e3120e45ff77b1799050ffd21694551753a
AlLixAI <157326564+AlLixAI@users.noreply.github.com>
2024-05-25 16:30:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,47 +9,46 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Функция для загрузки количества кликов с сервера
     function loadClicks() {
-    fetch('https://03a0-46-158-159-62.ngrok-free.app/get_clicks?telegram_id=' + userId)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Ошибка при загрузке кликов');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Обновляем счетчик кликов на фронтенде
-        scoreDisplay.textContent = data.clicks;
+        fetch('https://03a0-46-158-159-62.ngrok-free.app/get_clicks?telegram_id=' + userId)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при загрузке кликов');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Обновляем счетчик кликов на фронтенде
+            scoreDisplay.textContent = data.clicks;
 
-        // Добавляем обработчик события клика для кнопки
-        document.getElementById('clickButton').addEventListener('click', function() {
-            // Отправляем запрос на сервер для обновления счетчика кликов
-            fetch('https://03a0-46-158-159-62.ngrok-free.app/update_clicks', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ telegram_id: userId })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Ошибка при обновлении кликов');
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Обновляем счетчик кликов на фронтенде после успешного обновления
-                scoreDisplay.textContent = data.clicks;
-            })
-            .catch(error => {
-                console.error('Ошибка:', error);
+            // Добавляем обработчик события клика для кнопки
+            document.getElementById('clickButton').addEventListener('click', function() {
+                // Отправляем запрос на сервер для обновления счетчика кликов
+                fetch('https://03a0-46-158-159-62.ngrok-free.app/update_clicks', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ telegram_id: userId })
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Ошибка при обновлении кликов');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    // Обновляем счетчик кликов на фронтенде после успешного обновления
+                    scoreDisplay.textContent = data.clicks;
+                })
+                .catch(error => {
+                    console.error('Ошибка:', error);
+                });
             });
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
         });
-    })
-    .catch(error => {
-        console.error('Ошибка:', error);
-    });
-}
-
+    }
 
     // Проверка наличия Telegram WebApp API
     if (window.Telegram && window.Telegram.WebApp) {
